2f781d0aaa6fa8a9721d176fe9239cd05ea80310
Benjamin <benjamincliminio@gmail.com>
2023-07-26 03:49:32 -0300

    Finish the interface to Service, go to build the interface of donations

--- src/app/Componets/Footer.js
+++ src/app/Componets/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return (
       <div>
-       <footer className="border-t-2 mt-20 border-gray-300 ml-10 mr-10">
-       <p className="flex justify-center mt-5 font-bold text-lg text-gray-400">2023 &copy; NOMBRE</p>
+       <footer className="border-t-2 mt-20 border-gray-300 h-24 ml-10 mr-10">
+       <p className="flex justify-center mt-8 font-bold text-lg text-gray-400">2023 &copy; NOMBRE</p>
        </footer>
       </div>
     )
--- src/app/Service/page.js
+++ src/app/Service/page.js
@@ -1,4 +1,8 @@
 import NavBar from '../Componets/NavBar'
+import Footer from '../Componets/Footer'
+import { BsCamera } from 'react-icons/bs'
+import { CiLocationOn } from 'react-icons/ci'
+import { LiaAddressBookSolid } from 'react-icons/lia'
 
 function Service (){
     return(
@@ -10,15 +14,40 @@ function Service (){
       <section>
       <div>
        <p className="flex justify-center text-gray-400 mt-3 p-3 ml-6">Bueno, dejando de lado quienes somos pasemos a lo importante...</p>
-       <p className="flex justify-center text-2xl mt-5 mb-10">¿A que nos dedicamos?</p>
+       <p className="flex justify-center text-2xl mt-5 mb-10 font-bold">¿A que nos dedicamos?</p>
       </div>
       <div className="w-full h-36 bg-[#3A4F50] rounded-xl">
-       <p className="p-2 mt-2 ml-3">En (Nombre) ofrecemos un servicio en donde podes registrar a tu mascota/as creandoles su propio perfil y te brindamos un QR totalmente unico para tus peluditos. yo creo que no hace falta decir que es totalmente <span className='text-lg'>GRATIS</span></p>
+       <p className="p-2 mt-2 ml-3">En (Nombre) ofrecemos un servicio en donde podes registrar a tu mascota/as creandoles su propio perfil y te brindamos un QR totalmente unico para tus peluditos. yo creo que no hace falta decir que es totalmente <span className='text-lg text-[#0EF6CC]'>GRATIS</span></p>
       </div>
       <div>
        <p className='flex justify-center text-2xl mt-10'>Que le puedo cargar al perfil?</p>
-
       </div>
+      <div className='mt-5'>
+       <p className='flex justify-center text-2xl font-bold text-[#0EF6CC]'>#1</p>
+       <p className='flex justify-center font-bold mt-3'>Puedes subir Fotos de tu Peludito</p>
+      </div>
+      <div>
+      <BsCamera className='ml-5 -mt-7 text-[#0EF6CC]' size={30} />
+      </div>
+      <div className='mt-5'>
+       <p className='flex justify-center text-2xl font-bold text-[#0EF6CC]'>#2</p>
+       <p className='flex justify-center font-bold mt-3'>Puedes subir tu Direccion</p>
+      </div>
+      <div>
+      <CiLocationOn className='ml-4 -mt-7 text-[#0EF6CC]' size={35} />
+      </div>
+      <div className='mt-5'>
+       <p className='flex justify-center text-2xl font-bold text-[#0EF6CC]'>#3</p>
+       <p className='flex justify-center font-bold mt-3'>Puedes subir datos importantes</p>
+      </div>
+      <div>
+      <LiaAddressBookSolid className='ml-4 -mt-7 text-[#0EF6CC]' size={35} />
+      </div>
+      <p className="flex justify-center text-2xl font-bold mt-7 mb-10">¿Pero de que me sirve el QR?</p>
+      <div className="w-full h-40 bg-[#3A4F50] rounded-xl">
+       <p className="p-2 mt-2 ml-3">El QR va a servir para que lo imprimas y se lo pegues en el collar a tu mascota y en caso de que se pierda y alguien lo encuentre pueda escanear el QR y ver datos como direccion o numero de telefono para poder ayudar a un <span className='text-lg text-[#0EF6CC]'>Gran Rencuentro!!</span></p>
+      </div>
+      <Footer />
       </section>
      </div>
     )
